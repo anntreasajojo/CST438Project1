95ea5b17b516435ed8d82724b4f0682b87f6636e
fernandovelarde559-maker <fernandovelarde559@gmail.com>
2026-09-09 16:31:43 -0700

    Completed FoodDaoTest.kt, will fix remaining bug on line 26

--- app/src/androidTest/java/com/example/cst438project1/FoodDaoTest.kt
+++ app/src/androidTest/java/com/example/cst438project1/FoodDaoTest.kt
@@ -1,4 +1,46 @@
 package com.example.cst438project1
 
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.cst438project1.database.AppDatabase
+import com.example.cst438project1.database.Food
+import com.example.cst438project1.database.FoodDao
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
 class FoodDaoTest {
+    private lateinit var db: AppDatabase
+    private lateinit var foodDao: FoodDao
+
+    @Before
+    fun createDB() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        foodDao = db.foodDao()
+    }
+
+    @After
+    fun closeDB() {
+        try {
+            db.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @Test
+    fun insertUser() = runTest {
+        val food = Food(0, "Banana", 260, 0.4, 0.4, 0.4)
+        foodDao.insertFood(food)
+        val retrievedUser = foodDao.getFoodById(0)
+        assertEquals(food, retrievedUser)
+
+    }
 }
