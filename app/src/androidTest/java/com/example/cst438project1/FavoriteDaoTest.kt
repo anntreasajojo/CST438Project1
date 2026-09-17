45937864b755d46b20e448089c327a5332940694
fernandovelarde559-maker <fernandovelarde559@gmail.com>
2026-09-17 14:56:35 -0700

    Created FavoriteDaoTest.kt and added all the necessary imports i think

---
+++ app/src/androidTest/java/com/example/cst438project1/FavoriteDaoTest.kt
@@ -0,0 +1,19 @@
+package com.example.cst438project1
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.runner.RunWith
+import org.junit.Test
+import org.junit.Before
+import org.junit.After
+import org.junit.Assert.*
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.example.cst438project1.database.AppDatabase
+import com.example.cst438project1.database.Favorite
+import com.example.cst438project1.database.FavoriteDao
+import kotlinx.coroutines.test.runTest
+
+
+class FavoriteDaoTest {
+}
