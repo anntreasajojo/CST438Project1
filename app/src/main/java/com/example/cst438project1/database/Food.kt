bcb8a37cf5a3074bb7125db7a387d5b8585471df
fernandovelarde559-maker <fernandovelarde559@gmail.com>
2026-09-16 23:29:36 -0700

    Removed stuff from Food.kt, hopefully it passed the check thingy now

--- app/src/main/java/com/example/cst438project1/database/Food.kt
+++ app/src/main/java/com/example/cst438project1/database/Food.kt
@@ -5,7 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "foods")
-data class Food (
+data class Food(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
@@ -13,24 +13,4 @@ data class Food (
     val fat: Double,
     val protein: Double,
     val carbs: Double
-) {
-    override fun equals(other: Any?): Boolean {
-        return super.equals(other)
-    }
-
-    override fun hashCode(): Int {
-        return super.hashCode()
-    }
-
-    fun getId(): Int = id
-    fun getName(): String = name
-    fun getCalories(): Int = calories
-    fun getFat(): Double = fat
-    fun getProtein(): Double = protein
-    fun getCarbs(): Double = carbs
-
-    //Testing purposes
-    override fun toString(): String {
-        return "ID: $id\n Name: $name\n Calories: $calories\n Fat: $fat\n Protein: $protein\n Carbs: $carbs\n"
-    }
-}
+)
