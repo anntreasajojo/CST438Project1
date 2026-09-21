08c6124f8fba0c44bccb3c0d0dc38b5ecf2c3572
JayJauregui <jajauregui@csumb.edu>
2026-09-21 13:45:43 -0700

    Removed placeholder images and added more information when clicking on a food. It now shows value of Carbs, protein and Fat within the Categories Screen

--- app/src/main/java/com/example/cst438project1/Categories.kt
+++ app/src/main/java/com/example/cst438project1/Categories.kt
@@ -1,7 +1,9 @@
 package com.example.cst438project1
 
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
